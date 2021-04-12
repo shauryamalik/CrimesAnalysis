978b5292fc5b86fdc248a05897d99f95f0478994
shauryamalik <shauryamalik97@gmail.com>
2021-04-12 18:05:08 -0400

    updating filtered data

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -1,43 +1,133 @@
-var w = 700;
-var h = 600;
-var padding = 10;
+var boroughs = ["Bronx", "Brooklyn", "Staten Island", "Manhattan", "Queens"];
 
-var xScale = d3.scaleLinear()
-				.domain([-74.4,-73.6])
-				.range([0,w*0.8]);
-
-var yScale = d3.scaleLinear()
-				.domain([40.5, 40.9])
-				.range([h*0.8, padding])
-
-var yAxis = d3.axisLeft()
-				.scale(yScale);
-
-var xAxis = d3.axisBottom()
-				.scale(xScale);
-
-var svg = d3.select("div#plot")
-				.append("svg")
-				.attr("id", "demo")
-				.attr("width", w)
-				.attr("height", h);
-
-svg.append("g")
-	.attr("transform", `translate(${w*0.1}, ${padding})`)
-	.call(yAxis);
-
-svg.append("g")
-	.attr("transform", `translate(${w*0.1}, ${h*0.8+padding})`)
- 	.call(xAxis);
-
-datasrc = "https://raw.githubusercontent.com/shauryamalik/CrimesAnalysis/main/datasets/NYPD_Arrest_Data__Year_to_Date_.csv"
+datasrc = "https://raw.githubusercontent.com/shauryamalik/CrimesAnalysis/main/datasets/filtered/arrests_by_offenses_and_borough.csv";
 
 d3.csv(datasrc)
   .then(function(data) {
 	// stuff that requires the loaded data
-
+	var offenseMap = {};
+	data.forEach(function(d) {
+	    var offense = d.OFNS_DESC;
+	    offenseMap[offense] = [];//{};
+	    boroughs.forEach(function(field) {
+	    	var b_name = field;
+	    	offenseMap[offense][b_name]=[]
+	        offenseMap[offense].push( +d[field] );
+	    });
+	});
+	makeVis(offenseMap);
   })
   .catch(function(error) {
 		// error handling  
-  
   });
+
+var makeVis = function(offenseMap) {
+	var margin = { top: 30, right: 50, bottom: 30, left: 50 };
+    var width  = 700 - margin.left - margin.right;
+    var height = 600 - margin.top  - margin.bottom;
+
+	// var w = 700;
+	// var h = 600;
+	var padding = 10;
+
+	var xScale = d3.scaleBand()
+					.domain(boroughs)
+					.range([0,width]);
+
+	var yScale = d3.scaleLinear()
+					//.domain([40.5, 40.9])
+					.range([height, padding])
+
+	var yAxis = d3.axisLeft()
+					.scale(yScale);
+
+	var xAxis = d3.axisBottom()
+					.scale(xScale);
+
+	// // var svg = d3.select("div#plot")
+	// // 				.append("svg")
+	// // 				.attr("id", "demo")
+	// // 				.attr("width", w)
+	// // 				.attr("height", h);
+
+	// svg.append("g")
+	// 	.attr("transform", `translate(${w*0.1}, ${padding})`)
+	// 	.call(yAxis);
+
+	// svg.append("g")
+	// 	.attr("transform", `translate(${w*0.1}, ${h*0.8+padding})`)
+	//  	.call(xAxis);
+
+ 	// Create svg
+    var svg = d3.select("#vis-container")
+				      .append("svg")
+				        .attr("width",  width  + margin.left + margin.right)
+				        .attr("height", height + margin.top  + margin.bottom)
+				      .append("g")
+				        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    svg.append("g")
+    	.attr("class", "y axis")
+		.attr("transform", `translate(0, 0)`)
+		.call(yAxis);
+
+	var updateBars = function(data) {
+        // First update the y-axis domain to match data
+        yScale.domain( d3.extent(data) );
+        svg.append("g")
+	    	.attr("class", "y axis")
+			.attr("transform", `translate(0, 0)`)
+			.call(yAxis);
+
+        var bars = svg.selectAll(".bar").data(data);
+
+        // Add bars for new data
+        bars.enter()
+          .append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d,i) { return xScale( boroughs[i] )+55; })
+            .attr("width", 20)
+            .attr("y", function(d,i) { return yScale(d.Brooklyn); })
+            .attr("height", function(d,i) { return height; }); // - yScale(d.Brooklyn)
+
+        // Update old ones, already have x / width from before
+        bars
+            .transition().duration(250)
+            .attr("y", function(d,i) { return yScale(d.Brooklyn); })
+            .attr("height", function(d,i) { return height; }); // - yScale(d.Brooklyn)
+
+        // Remove old ones
+        bars.exit().remove();
+    };
+
+    // Handler for dropdown value change
+    var dropdownChange = function() {
+        var newOffense = d3.select(this).property('value'),
+            newData   = offenseMap[newOffense];
+
+        updateBars(newData);
+    };
+
+    // Get names of offenses, for dropdown
+    var offenses = Object.keys(offenseMap).sort();
+
+    var dropdown = d3.select("#vis-container")
+        .insert("select", "svg")
+        .on("change", dropdownChange);
+
+    dropdown.selectAll("option")
+        .data(offenses)
+        .enter().append("option")
+        .attr("value", function (d) { return d; })
+        .text(function (d) {
+            return d;//[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+        });
+
+    var initialData = offenseMap[ offenses[0] ];
+    updateBars(initialData);
+};
